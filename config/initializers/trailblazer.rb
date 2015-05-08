bad57b5fb13a52ef2a719911f1411721d289d6a6
Jason Elston <jason@onezeroone.com.au>
2015-05-08 23:33:01 +1000

    Added fix for issue with undefined method 'reforms_path' error

--- config/initializers/trailblazer.rb
+++ config/initializers/trailblazer.rb
@@ -1,1 +1,13 @@
 require 'trailblazer/autoloading'
+
+# Required to eliminate to avoid a undefined method 'reforms_path' error
+# for now. This will be done automatically soon.
+Trailblazer::Operation::CRUD.module_eval do
+  module Included
+    def included(base)
+      super # the original CRUD::included method.
+      base.send :include, Trailblazer::Operation::CRUD::ActiveModel
+    end
+  end
+  extend Included # override CRUD::included.
+end
